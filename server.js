20d7a8aec5f061b077d7f2aac36288d76a3c2ab8
Jimmy Alford <jalford14@liberty.edu>
2019-07-08 11:14:32 -0400

    Initial routing

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+var routes = require('./app/api/routes');
 
+app.use('/api', routes);
 app.listen(port);
 
-console.log('todo list RESTful API server started on: ' + port);
+console.log('todo list restful api server started on: ' + port);
